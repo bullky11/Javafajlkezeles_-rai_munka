4e64d9275d8a97aaf4b6a32dfbb349ec3d801b55
Pozsa Marcell (SZF_E_2022_2024) <pozsa.marcell@diak.szamalk-szalezi.hu>
2023-05-18 18:32:45 +0200

    6-7 feladat  hozzáadása

--- src/progtetelek/FileesAdatszerkezetek.java
+++ src/progtetelek/FileesAdatszerkezetek.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.ParseException;
 import java.util.List;
+import java.text.DecimalFormat;
+import java.util.Locale;
 
 public class FileesAdatszerkezetek {
 
@@ -17,10 +19,10 @@ public class FileesAdatszerkezetek {
 
     public FileesAdatszerkezetek() throws IOException, ParseException {
         sorok = Files.readAllLines(Path.of("fuvar.csv"));
-         fuvarok = new Fuvar[sorok.size() - 1];
+        fuvarok = new Fuvar[sorok.size() - 1];
         for (int i = 1; i < sorok.size(); i++) {
 
-            fuvarok[i-1] = new Fuvar(sorok.get(i));
+            fuvarok[i - 1] = new Fuvar(sorok.get(i));
 
         }
         assert sorok.size() - 1 == fuvarok.length : "nincs meg minden fuvar!";
@@ -34,6 +36,9 @@ public class FileesAdatszerkezetek {
         feladat3();
         feladat4();
         feladatok5();
+        feladatok6();
+        feladatok7();
+        
     }
 
     private void feladat1() throws IOException {
@@ -63,17 +68,38 @@ public class FileesAdatszerkezetek {
     private void feladat4() throws ParseException {
         System.out.println("milyen dátumú a legnagyobb távolságú fuvar");
         //maxkiválasztás tétele.
-       
-        int index=0;
-        for (int i = 1; i < fuvarok.length-1; i++) {
-            if (fuvarok[i].getTavolsag()> fuvarok[i].getTavolsag()) {
-                index=i;   
+
+        int index = 0;
+        for (int i = 1; i < fuvarok.length - 1; i++) {
+            if (fuvarok[i].getTavolsag() > fuvarok[i].getTavolsag()) {
+                index = i;
             }
         }
-        System.out.println("távolság: "+ fuvarok[index].getTavolsag());
-        System.out.println("dátum: "+ fuvarok[index].getIndulas());
+        System.out.println("távolság: " + fuvarok[index].getTavolsag());
+        System.out.println("dátum: " + fuvarok[index].getIndulas());
     }
 
     private void feladatok5() {
+        System.out.println("borravalók átlaga");
+        double osszeg=0;
+        for (int i = 0; i < fuvarok.length; i++) {
+            osszeg+=fuvarok[i].getBorravalo();
+        }
+        Locale loc = Locale.ENGLISH;
+        System.out.printf(loc,"%.2f \n ", osszeg/fuvarok.length);
+    }
+
+    private void feladatok6() {
+        System.out.println("6.feladat minden fizetés bankkártyás");
+        int i=0, N=fuvarok.length;
+        while (i < N && fuvarok[i].getFizetes_modja().equals("bankkártya")) {            
+            i++;
+        }
+        boolean mind= i>N;
+        System.out.println(mind +" index:  "+i+" "+fuvarok[i].getFizetes_modja());
+    }
+
+    private void feladatok7() {
+        
     }
 }
